158a3526ec3590e6fec6c1016fe379e70e1687c6
Bolinas Frank <bo@bofrank.com>
2019-07-10 16:02:49 -0700

    Starting to put form into React.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 
+handleChange(event) {
+  console.log('call api');
+}
+
 function Hi() {
   return (
     <div className="section-header">
@@ -11,6 +15,25 @@ function Hi() {
         <div id="hero-gradient" />
         <div id="hero-search">
           <h2>Discover the best recipes</h2>
+          <div id="search" className="container-fluid container-constrain">
+            <div onKeyPress="Javascript: if (event.keyCode==13) load();">
+              <div className="row row-condensed">
+                <div className="col-sm-12">
+                  <div className="form-element">
+                    <input
+                      name="query"
+                      id="ingredient"
+                      className="form-control input-lg form-control-icon icon-location"
+                      type="text"
+                      placeholder="What ingredient do you want to use?"
+                      value=""
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
