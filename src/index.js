ad300584e6445d191c760f600838e7ffd0e72bb0
Bolinas Frank <bo@bofrank.com>
2019-07-10 15:48:57 -0700

    Restructuring assets and adding hero to React.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import { render } from "react-dom";
 
 function Hi() {
   return (
-    <div>
-      <img
-        src="images/cabinet-contemporary-counter-1080721.jpg"
-        alt="Photo by Mark McCammon from Pexels"
-      />
+    <div className="section-header">
+      <div id="hero" className="section-home">
+        <div id="hero-bg">
+          <div className="bg" alt="Photo by Mark McCammon from Pexels" />
+        </div>
+        <div id="hero-gradient" />
+        <div id="hero-search">
+          <h2>Discover the best recipes</h2>
+        </div>
+      </div>
     </div>
   );
 }
