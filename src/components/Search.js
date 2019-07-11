b0dbb046c48dcf99c1e36a118baac849bd1b59fb
Bolinas Frank <bo@bofrank.com>
2019-07-10 20:20:50 -0700

    Hooking up function to call api.

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,6 +11,109 @@ class Search extends React.Component {
     this.setState({ recipe });
   };
 
+  handleSubmit = event => {
+    event.preventDefault();
+    //this.props.actions.createCourse(this.state.course);
+    console.log(this.state.recipe.ingredient);
+    this.load();
+  };
+
+  load = () => {
+    document.getElementById("grid").innerHTML = "";
+    document.getElementById("loading").style.display = "block";
+
+    function checkComplete(imageCount, recipeCount) {
+      if (imageCount == recipeCount) {
+        this.resizeAllGridItems();
+      }
+    }
+
+    var ingredient = document.getElementById("ingredient").value;
+    var appkey = "f3060af875eda52baf4d68b1fdbdbf43";
+    var appid = "5fb37b30";
+    var recipeUrl = `https://api.edamam.com/search?app_id=${appid}&app_key=f3060af875eda52baf4d68b1fdbdbf43&q=${ingredient}`;
+
+    fetch(recipeUrl)
+      .then(response => {
+        return response.json();
+      })
+      .then(edamamJSON => {
+        let node;
+        let element;
+        let recipeHTML;
+        let ingredientsHTML = "";
+
+        if (edamamJSON.hits.length > 0) {
+          for (var k = 0; k < edamamJSON.hits.length; k++) {
+            for (
+              var i = 0;
+              i < edamamJSON.hits[k].recipe.ingredients.length;
+              i++
+            ) {
+              ingredientsHTML +=
+                '<div itemprop="recipeIngredient">' +
+                edamamJSON.hits[k].recipe.ingredients[i].text +
+                "</div>";
+            }
+
+            recipeHTML =
+              '<a itemscope itemtype="http://schema.org/Recipe" id="recipe"' +
+              k +
+              ' class="item photo" href="' +
+              edamamJSON.hits[k].recipe.url +
+              '" target="_blank"><div class="content"><img itemprop="image" id="image0"' +
+              k +
+              ' class="grid-item-image" src="' +
+              edamamJSON.hits[k].recipe.image +
+              '" onload="resizeAllGridItems()" /><div itemprop="name" id="title0"' +
+              k +
+              ' class="grid-item-title">' +
+              edamamJSON.hits[k].recipe.label +
+              '</div><div class="grid-text">' +
+              ingredientsHTML +
+              "</div></div></a>";
+
+            document.getElementById("grid").innerHTML += recipeHTML;
+
+            ingredientsHTML = "";
+          }
+        } else {
+          document.getElementById("grid").innerHTML +=
+            " " + ingredient + " is not found in our recipe library :(";
+        }
+      })
+      .then(function(edamamJSON) {
+        document.getElementById("loading").style.display = "none";
+      })
+      .catch(function() {
+        console.log("fetch failed");
+      });
+
+    return false;
+  };
+
+  resizeGridItem = item => {
+    var grid = document.getElementsByClassName("grid")[0];
+    var rowHeight = parseInt(
+      window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
+    );
+    var rowGap = parseInt(
+      window.getComputedStyle(grid).getPropertyValue("grid-row-gap")
+    );
+    var rowSpan = Math.ceil(
+      (item.querySelector(".content").getBoundingClientRect().height + rowGap) /
+        (rowHeight + rowGap)
+    );
+    item.style.gridRowEnd = "span " + rowSpan;
+  };
+
+  resizeAllGridItems = () => {
+    var allItems = document.getElementsByClassName("item");
+    for (var x = 0; x < allItems.length; x++) {
+      this.resizeGridItem(allItems[x]);
+    }
+  };
+
   render() {
     return (
       <div className="section-header">
@@ -22,7 +125,11 @@ class Search extends React.Component {
           <div id="hero-search">
             <h2>Discover the best recipes</h2>
             <div id="search" className="container-fluid container-constrain">
-              <div>
+              <div
+                onKeyPress={() => {
+                  if (event.keyCode == 13) this.handleSubmit(event);
+                }}
+              >
                 <div className="row row-condensed">
                   <div className="col-sm-12">
                     <div className="form-element">
