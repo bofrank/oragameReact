6ae814254d4d3371622d39a6a15739b929e90c4e
Bolinas Frank <bo@bofrank.com>
2020-05-14 19:51:38 -0700

    Adding affiliate promo.

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,7 +18,7 @@ function Search() {
     document.getElementById("grid").innerHTML = "";
     document.getElementById("loading").style.display = "block";
 
-    var ingredient = document.getElementById("ingredient").value;
+    let ingredient = document.getElementById("ingredient").value;
     var appid = "5fb37b30";
     var recipeUrl = `https://api.edamam.com/search?app_id=${appid}&app_key=f3060af875eda52baf4d68b1fdbdbf43&q=${ingredient}`;
 
@@ -31,8 +31,12 @@ function Search() {
         let ingredientsHTML = "";
         let promoHTML;
 
-        promoHTML =
-          '<a itemscope itemtype="http://schema.org/Recipe" id="promo" class="item photo" href="https://www.kqzyfj.com/click-7903156-14041151" target="_blank"><div class="content"><img itemprop="image" id="imagePromo" class="grid-item-image" src="https://www.lduhtrp.net/image-7903156-14041151" onload="resizeAllGridItems()" /><div itemprop="name" id="titlePromo" class="grid-item-title">Up to 25% off</div><div class="grid-text"><div itemprop="recipeIngredient">Serious deals on kitchen, baking, cooking, appliances, kitchenaid, countertop appliances, stand mixers, blenders, coffee makers, food processors attachments, accessories, toasters, slow cookers here!</div></div></div></a>'
+        if (ingredient == "salmon") {
+          promoHTML = '<a itemscope itemtype="http://schema.org/Recipe" id="promo" class="item photo" href="https://www.jdoqocy.com/click-7903156-14026812" target="_blank"><div class="content"><img itemprop="image" id="imagePromo" class="grid-item-image" src="https://www.ftjcfx.com/image-7903156-14026812" onload="resizeAllGridItems()" /><div itemprop="name" id="titlePromo" class="grid-item-title">FREE Shipping</div><div class="grid-text"><div itemprop="recipeIngredient">Free Shipping On Wild Seafood & Organic Fare!</div></div></div></a>'
+        } else {
+          promoHTML =
+            '<a itemscope itemtype="http://schema.org/Recipe" id="promo" class="item photo" href="https://www.kqzyfj.com/click-7903156-14041151" target="_blank"><div class="content"><img itemprop="image" id="imagePromo" class="grid-item-image" src="https://www.lduhtrp.net/image-7903156-14041151" onload="resizeAllGridItems()" /><div itemprop="name" id="titlePromo" class="grid-item-title">Up to 25% off</div><div class="grid-text"><div itemprop="recipeIngredient">Serious deals on kitchen, baking, cooking, appliances, kitchenaid, countertop appliances, stand mixers, blenders, coffee makers, food processors attachments, accessories, toasters, slow cookers here!</div></div></div></a>'
+        }
 
         document.getElementById("grid").innerHTML += promoHTML;
 
